8098b391f6c8b919194d36109ff1cc3f24af999c
Veronica Stenberg <Veronica.Stenberg@amf.se>
2018-03-07 14:30:20 +0100

    Lagt till kod

--- src/fil2.sql
+++ src/fil2.sql
@@ -1,1 +1,8 @@
 Detta är en superjättebrajättebra SQL-fil hoppas jag! Ojdå! DET BLEV FEL!!!
+Nu lägger vi till rader
+
+Lite programmering kanske?
+
+SELECT *
+FROM DW_Martar.amf.Dimension_Försäkring
+WHERE DW_Giltig_Tom = '9999-12-31'
